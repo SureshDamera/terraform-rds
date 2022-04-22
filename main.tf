d2dd46df814ecf1fb2afe16caf52795c575c9f11
Suresh Damera <suresh.damera@innomate.com.au>
2022-04-22 19:04:10 +0530

    using random password

--- main.tf
+++ main.tf
@@ -105,7 +105,7 @@ resource "aws_secretsmanager_secret_version" "onmostealth_rds_credentials" {
   secret_string = <<EOF
 {
   "username": "${var.onmostealth_username}",
-  "password": random_password.onmoauth_master_password.result,
+  "password": "${random_password.onmoauth_master_password.result},
   "engine": "mysql",
   "host": "${aws_rds_cluster.onmostealth-aurora-cluster.endpoint}",
   "port": ${var.onmostealth_port},
